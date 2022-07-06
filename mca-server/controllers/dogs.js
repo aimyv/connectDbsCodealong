ed018e902e0982ae160ea70fb7796d186ade5c96
Tom Crowther <tom.crowther1992@gmail.comgit config --global user.name Crowy92>
2022-07-06 12:12:09 +0100

    made controller synchronous

--- mca-server/controllers/dogs.js
+++ mca-server/controllers/dogs.js
@@ -2,14 +2,9 @@ const router = require('express').Router();
 
 const Dog = require('../models/dog');
 
-router.get('/', async (req, res) => {
-    try {
-        const dogs = await Dog.all
-        res.status(200).json({data: dogs})
-    } catch(err) {
-        console.error(err);
-        res.status(500).json({ error: err })
-    }
+router.get('/', (req, res) => {
+    const dogs = Dog.all
+    res.status(200).json({data: dogs})
 });
 
 module.exports = router
